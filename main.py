246f190c09b0424ff723e4c572975848b00ad283
Mc Nigno <mcnigno@gmail.com>
2021-02-18 01:33:11 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from openpyxl import load_workbook
+import pymysql
+'''
+'db_user_name' => 'suite_user',
+'db_password' => 'lollipop300777',
+'db_name' => 'suitecrm',
+'''
+# Open database connection
+db = pymysql.connect("localhost","suite_user","lollipop300777","suitecrm" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+result = cursor.execute("SELECT * from project_task limit 100")
+print(result)
+# disconnect from server
+db.close()
